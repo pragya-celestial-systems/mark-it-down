c8d8394731b7258e7059189d2955b9e6424d42a0
pragya-celestial-systems <pragya.saxena@celestialsys.com>
2024-11-16 23:58:44 +0530

    Feat : Display custom context menu on right click

--- src/components/FileButton.jsx
+++ src/components/FileButton.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./css/file.button.module.css";
 import { useTextAreaContext } from "../context/TextAreaContext";
 
 function FileButton({ fileData }) {
   const { setValue } = useTextAreaContext();
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const [menuStyles, setMenuStyles] = useState({});
+
+  useEffect(() => {
+    function handleCloseMenu() {
+      setDisplayMenu(false);
+    }
+
+    window.addEventListener("click", handleCloseMenu);
+
+    return () => {
+      window.removeEventListener("click", handleCloseMenu);
+    };
+  }, []);
 
   function handleDisplayPreview() {
     setValue(fileData.readmeFile);
   }
 
+  function handleDisplayContextMenu(event) {
+    event.preventDefault();
+    const { clientX, clientY } = event;
+    setDisplayMenu(true);
+    setMenuStyles({ top: clientY, left: clientX });
+  }
+
   return (
-    <div id={styles.container} onClick={handleDisplayPreview}>
+    <div
+      id={styles.container}
+      onClick={handleDisplayPreview}
+      onContextMenu={handleDisplayContextMenu}
+    >
       <h3 id={styles.fileName}>{`File-${fileData.id}`}</h3>
+      {displayMenu && (
+        <div
+          style={menuStyles}
+          id={styles.contextMenu}
+          onClick={(e) => e.stopPropagation()}
+        >
+          Hello world
+        </div>
+      )}
     </div>
   );
 }
